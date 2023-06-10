afab09f00bff49d6d84635a863809a53ca23342f
Rey12456 <124766416+Rey12456@users.noreply.github.com>
2023-06-10 10:49:38 -0500

    Update forms.py

--- account/forms.py
+++ account/forms.py
@@ -1,10 +1,9 @@
 from django import forms
-from django.contrib.auth.forms import (AuthenticationForm, PasswordResetForm,
-                                       SetPasswordForm)
+from django.contrib.auth.forms import (AuthenticationForm, PasswordResetForm, SetPasswordForm)
 
 from .models import UserBase
 
-
+# Form for user login
 class UserLoginForm(AuthenticationForm):
 
     username = forms.CharField(widget=forms.TextInput(
@@ -17,7 +16,7 @@ class UserLoginForm(AuthenticationForm):
         }
     ))
 
-
+# Form for user registration
 class RegistrationForm(forms.ModelForm):
 
     user_name = forms.CharField(
@@ -33,6 +32,7 @@ class RegistrationForm(forms.ModelForm):
         fields = ('user_name', 'email',)
 
     def clean_user_name(self):
+        # Ensure the username is unique
         user_name = self.cleaned_data['user_name'].lower()
         r = UserBase.objects.filter(user_name=user_name)
         if r.count():
@@ -40,20 +40,22 @@ class RegistrationForm(forms.ModelForm):
         return user_name
 
     def clean_password2(self):
+        # Ensure the passwords match
         cd = self.cleaned_data
         if cd['password'] != cd['password2']:
             raise forms.ValidationError('Passwords do not match.')
         return cd['password2']
 
     def clean_email(self):
+        # Ensure the email is unique
         email = self.cleaned_data['email']
         if UserBase.objects.filter(email=email).exists():
-            raise forms.ValidationError(
-                'Please use another Email, that is already taken')
+            raise forms.ValidationError('Please use another Email, that is already taken')
         return email
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        # Set widget attributes for styling
         self.fields['user_name'].widget.attrs.update(
             {'class': 'form-control mb-3', 'placeholder': 'Username'})
         self.fields['email'].widget.attrs.update(
@@ -63,21 +65,21 @@ class RegistrationForm(forms.ModelForm):
         self.fields['password2'].widget.attrs.update(
             {'class': 'form-control', 'placeholder': 'Repeat Password'})
 
-
+# Form for password reset
 class PwdResetForm(PasswordResetForm):
 
     email = forms.EmailField(max_length=254, widget=forms.TextInput(
         attrs={'class': 'form-control mb-3', 'placeholder': 'Email', 'id': 'form-email'}))
 
     def clean_email(self):
+        # Ensure the email exists
         email = self.cleaned_data['email']
         u = UserBase.objects.filter(email=email)
         if not u:
-            raise forms.ValidationError(
-                'Unfortunatley we can not find that email address')
+            raise forms.ValidationError('Unfortunately, we cannot find that email address')
         return email
 
-
+# Form for password reset confirmation
 class PwdResetConfirmForm(SetPasswordForm):
     new_password1 = forms.CharField(
         label='New password', widget=forms.PasswordInput(
@@ -86,11 +88,11 @@ class PwdResetConfirmForm(SetPasswordForm):
         label='Repeat password', widget=forms.PasswordInput(
             attrs={'class': 'form-control mb-3', 'placeholder': 'New Password', 'id': 'form-new-pass2'}))
 
-
+# Form for user profile editing
 class UserEditForm(forms.ModelForm):
 
     email = forms.EmailField(
-        label='Account email (can not be changed)', max_length=200, widget=forms.TextInput(
+        label='Account email (cannot be changed)', max_length=200, widget=forms.TextInput(
             attrs={'class': 'form-control mb-3', 'placeholder': 'email', 'id': 'form-email', 'readonly': 'readonly'}))
 
     user_name = forms.CharField(
@@ -107,5 +109,6 @@ class UserEditForm(forms.ModelForm):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        # Set required fields
         self.fields['user_name'].required = True
         self.fields['email'].required = True
