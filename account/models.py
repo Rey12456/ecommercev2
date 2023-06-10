a4a8f7a9da63d5f67d82eb97fec35338492388f5
Rey12456 <124766416+Rey12456@users.noreply.github.com>
2023-06-10 10:50:43 -0500

    Update models.py

--- account/models.py
+++ account/models.py
@@ -7,42 +7,48 @@ from django_countries.fields import CountryField
 
 
 class CustomAccountManager(BaseUserManager):
+    """
+    Custom manager for the user model.
+    """
 
     def create_superuser(self, email, user_name, password, **other_fields):
-
+        """
+        Creates and saves a new superuser with the given details.
+        """
         other_fields.setdefault('is_staff', True)
         other_fields.setdefault('is_superuser', True)
         other_fields.setdefault('is_active', True)
 
         if other_fields.get('is_staff') is not True:
-            raise ValueError(
-                'Superuser must be assigned to is_staff=True.')
+            raise ValueError('Superuser must be assigned to is_staff=True.')
         if other_fields.get('is_superuser') is not True:
-            raise ValueError(
-                'Superuser must be assigned to is_superuser=True.')
+            raise ValueError('Superuser must be assigned to is_superuser=True.')
 
         return self.create_user(email, user_name, password, **other_fields)
 
     def create_user(self, email, user_name, password, **other_fields):
-
+        """
+        Creates and saves a new user with the given details.
+        """
         if not email:
             raise ValueError(_('You must provide an email address'))
 
         email = self.normalize_email(email)
-        user = self.model(email=email, user_name=user_name,
-                          **other_fields)
+        user = self.model(email=email, user_name=user_name, **other_fields)
         user.set_password(password)
         user.save()
         return user
 
 
 class UserBase(AbstractBaseUser, PermissionsMixin):
+    """
+    Custom user model.
+    """
 
     email = models.EmailField(_('email address'), unique=True)
     user_name = models.CharField(max_length=150, unique=True)
     first_name = models.CharField(max_length=150, blank=True)
-    about = models.TextField(_(
-        'about'), max_length=500, blank=True)
+    about = models.TextField(_('about'), max_length=500, blank=True)
     # Delivery details
     country = CountryField()
     phone_number = models.CharField(max_length=15, blank=True)
@@ -66,6 +72,9 @@ class UserBase(AbstractBaseUser, PermissionsMixin):
         verbose_name_plural = "Accounts"
 
     def email_user(self, subject, message):
+        """
+        Sends an email to the user.
+        """
         email = EmailMessage(
             subject=subject,
             body=message,
@@ -73,7 +82,6 @@ class UserBase(AbstractBaseUser, PermissionsMixin):
             to=[self.email],
         )
         email.send()
-
 
     def __str__(self):
         return self.user_name
